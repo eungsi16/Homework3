80e52857a8b2918cc7f7b2169e1773554ee95b44
EunmiLee <eunmi984@gmail.com>
2017-12-13 15:00:47 +0900

    구글 맵이용 현재위치 및 위치검색 BY.이은미

---
+++ app/src/main/java/com/hansung/android/homework3/RestaurantMapActivity.java
@@ -0,0 +1,160 @@
+package com.hansung.android.homework3;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class RestaurantMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    final private int REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION = 100;
+    final private int REQUEST_PERMISSIONS_FOR_LOCATION_UPDATES = 101;
+    private Location mLastLocation;
+    private GoogleMap mgoogleMap;
+    Address address;
+    EditText editText;
+    String ed;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_map);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (!checkLocationPermissions()) {
+            requestLocationPermissions(REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION);
+        } else{
+            getLastLocation();
+        }
+
+        Button btn = (Button) findViewById(R.id.b1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mgoogleMap != null) {
+                    editText = (EditText) findViewById(R.id.edit);
+                    ed = editText.getText().toString();
+                    getAddress();
+                    LatLng location = new LatLng(address.getLatitude(), address.getLongitude());
+
+                    mgoogleMap.addMarker(
+                            new MarkerOptions().
+                                    position(location).
+                                    title(ed)
+                    );
+                    mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+                }
+            }
+        });
+    }
+
+    private boolean checkLocationPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermissions(int requestCode) {
+        ActivityCompat.requestPermissions(
+                RestaurantMapActivity.this,            // MainActivity 액티비티의 객체 인스턴스를 나타냄
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},        // 요청할 권한 목록을 설정한 String 배열
+                requestCode    // 사용자 정의 int 상수. 권한 요청 결과를 받을 때
+        );
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // getLastLocation();
+                } else {
+                    Toast.makeText(this, "Permission required", Toast.LENGTH_SHORT);
+                }
+                break;
+            }
+            case REQUEST_PERMISSIONS_FOR_LOCATION_UPDATES: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //startLocationUpdates();
+                } else {
+                    Toast.makeText(this, "Permission required", Toast.LENGTH_SHORT);
+                }
+            }
+        }
+    }
+    @SuppressWarnings("MissingPermission")
+    private void getLastLocation() {
+        Task task = mFusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    mLastLocation = location;
+                    LatLng Llocation = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+                    mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Llocation,15));
+
+                    //updateUI();
+                } else
+                    Toast.makeText(getApplicationContext(), getString(R.string.no_location_detected), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mgoogleMap = googleMap;
+    }
+
+    private void getAddress() {
+        TextView TextView = (TextView) findViewById(R.id.result);
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.KOREA);
+            List<Address> addresses = geocoder.getFromLocationName(ed, 1);
+            if (addresses.size() > 0) {
+                address = (Address) addresses.get(0);
+
+                TextView.setText(String.format("[ %s , %s ]",
+                        address.getLatitude(),
+                        address.getLongitude()));
+            }
+        } catch (IOException e) {
+            Log.e(getClass().toString(), "Failed in using Geocoder.", e);
+            return;
+        }
+    }
+
+}
